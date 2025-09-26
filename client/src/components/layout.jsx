fa998186f369b4105f5d55df7859f931abc3e49c
Jkyle <jkyletaghoy@gmail.com>
2025-09-26 15:07:50 +0800

    refactor: clean up navbar component by removing unused code and comments

--- client/src/components/layout.jsx
+++ client/src/components/layout.jsx
@@ -4,27 +4,30 @@ import { Outlet } from "react-router-dom";
 import SidebarUser from "./sidebaruser";
 import SidebarAdmin from "./sidebaradmin";
 import NavBar from "./navbar";
+import "./layout.css";
 
 function Layout({ role }) {
   const [searchResults, setSearchResults] = useState(null);
 
   return (
-    <div className="container-fluid vh-50 d-flex flex-column">
-      <div className="row flex-grow-1">
-        {/* Navbar now passes results up */}
-        <NavBar onSearch={setSearchResults} />
+    <div className="layout d-flex flex-column vh-100">
+      {/* Navbar at the top */}
+      <NavBar onSearch={setSearchResults} />
 
-        {/* Sidebar changes depending on role */}
-        {role === "admin" ? <SidebarAdmin /> : <SidebarUser />}
-
-        {/* Page Content gets both search results and default content */}
-        <div className="col p-4">
-          <Outlet context={{ searchResults }} />
+      {/* Main content area (sidebar + page content) */}
+      <div className="d-flex flex-grow-1 overflow-hidden">
+        {/* Sidebar (switches by role) */}
+        <div className="sidebar-wrapper">
+          {role === "admin" ? <SidebarAdmin /> : <SidebarUser />}
         </div>
+
+        {/* Page Content */}
+        <main className="content-area flex-grow-1 p-4 overflow-auto">
+          <Outlet context={{ searchResults }} />
+        </main>
       </div>
     </div>
   );
 }
 
 export default Layout;
-  
